e928c3650f390ec603c25f4a2e74db27e29750b3
Mat Schaffer <mat.schaffer@gmail.com>
2009-10-22 11:10:31 -0400

    Basic query and data display functionality.

---
+++ demo.js
@@ -0,0 +1,47 @@
+var db = openDatabase("sqlDemo", "1.0", "A database for doing sql demos.");
+
+function header(rows) {
+  var headers = ["<thead><tr>"];
+  for (i in rows.item(0)) {
+    headers.push(["<th>", i, "</th>"].join(''));
+  }
+  headers.push("</tr></thead>");
+  return headers.join('');  
+}
+
+function body(rows) {
+  var b = ["<tbody>"];
+  for (var i = 0; i < rows.length; i++) {
+    var item = rows.item(i);
+    b.push("<tr>");
+    for (key in item) {
+      b.push("<td>", item[key], "</td>");      
+    }
+    b.push("</tr>");
+  }
+  b.push("</tbody>");
+  return b.join('');
+}
+
+function s(query, params) {
+  db.transaction(function(tx) {
+    tx.executeSql(query, params, function(tx, results) {
+      var rows = results.rows;
+      if (rows.length > 0) {
+        var t = $('#results');
+        t.html(header(rows));
+        t.append(body(rows));     
+      }
+      console.log(results.rowsAffected + " rows affected.");
+    },
+    function(tx, e) {
+      console.error(["Query Error ", "[", e.code , "]:  ", e.message].join(''));
+    });
+  }, 
+  function(e) {
+    console.error(["Transaction Error ", "[", e.code , "]:  ", e.message].join(''));
+  },
+  function() {
+    console.info("Transaction complete");
+  });
+}
